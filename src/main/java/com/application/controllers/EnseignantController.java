2c0c4f9b83374dd581fef42198c4f155e5d09e80
midosdm <mohamedsaidem01@gmail.com>
2022-03-09 15:03:42 +0100

    created services implementation package

--- src/main/java/com/application/controllers/EnseignantController.java
+++ src/main/java/com/application/controllers/EnseignantController.java
@@ -1,11 +1,9 @@
 package com.application.controllers;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.application.services.EnseignantServiceImp;
+import com.application.services.Impl.EnseignantServiceImp;
 
 @RestController
 @RequestMapping("/enseignants")
