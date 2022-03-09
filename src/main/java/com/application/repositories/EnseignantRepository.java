692b837fbfb93329123468e8780ff09361055cee
midosdm <mohamedsaidem01@gmail.com>
2022-03-09 14:01:48 +0100

    added promotionpk to repository

--- src/main/java/com/application/repositories/EnseignantRepository.java
+++ src/main/java/com/application/repositories/EnseignantRepository.java
@@ -1,11 +1,12 @@
 package com.application.repositories;
 
+import com.application.models.PromotionPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.application.models.Enseignant;
 
 @Repository
-public interface EnseignantRepository extends JpaRepository<Enseignant,Integer> {
+public interface EnseignantRepository extends JpaRepository<Enseignant, PromotionPK> {
 
 }
