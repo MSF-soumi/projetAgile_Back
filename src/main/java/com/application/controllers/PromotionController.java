98354e5adc3bd0deeebc83556a716d13be39680d
DEV2 <mchachti.soumiya@gmail.com>
2022-03-11 11:12:44 +0100

    getById and swagger

--- src/main/java/com/application/controllers/PromotionController.java
+++ src/main/java/com/application/controllers/PromotionController.java
@@ -1,8 +1,15 @@
 package com.application.controllers;
 
+import com.application.dto.EnseignantDTO;
 import com.application.dto.PromotionDTO;
 import com.application.models.Promotion;
+import com.application.models.PromotionPK;
 import com.application.services.PromotionService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,20 +35,42 @@ public class PromotionController {
         this.modelMapper = modelMapper;
         this.promotionService = promotionService;
     }
-
+	@ApiOperation(value="Lister toutes les promotions")
+	@ApiResponses(value= {
+			@ApiResponse(code=200,message="Requette réussie"),
+			@ApiResponse(code=500,message="Erreur serveur, Reessayez!"),
+			@ApiResponse(code=400,message="Requette non réussie")
+	})
     @GetMapping
     public List<PromotionDTO> getAll(){
         var promotions = promotionService.getAll();
         return promotions.stream().map(this::convertToDto).collect(Collectors.toList());
     }
-
+	@ApiOperation(value="Créer une promotion")
+	@ApiResponses(value= {
+			@ApiResponse(code=200,message="Requette réussie"),
+			@ApiResponse(code=500,message="Erreur serveur, Reessayez!"),
+			@ApiResponse(code=400,message="Requette non réussie")
+	})
     @PostMapping()
     public PromotionDTO create(@Valid @RequestBody PromotionDTO promotionDTO){
         var promotion = convertToEntity(promotionDTO);
         var newPromotion = promotionService.create(promotion);
         return convertToDto(newPromotion);
     }
-
+    
+	@ApiOperation(value="Rechercher une promotion par ID")
+	@ApiResponses(value= {
+			@ApiResponse(code=200,message="Requette réussie"),
+			@ApiResponse(code=500,message="Erreur serveur, Reessayez!"),
+			@ApiResponse(code=400,message="Requette non réussie")
+	})
+	@GetMapping(path = "/{code_Formation}/{annee_Universitaire}")
+	public PromotionDTO getById(@PathVariable String code_Formation,@PathVariable String annee_Universitaire){
+		PromotionPK id= new PromotionPK(code_Formation,annee_Universitaire);
+		var promotion = promotionService.getById(id);
+		return this.convertToDto(promotion);
+	}
     private PromotionDTO convertToDto(Promotion promotion) {
         return modelMapper.map(promotion, PromotionDTO.class);
     }
--- src/main/java/com/application/services/PromotionService.java
+++ src/main/java/com/application/services/PromotionService.java
@@ -1,6 +1,7 @@
 package com.application.services;
 
 import com.application.models.Promotion;
+import com.application.models.PromotionPK;
 
 import java.util.List;
 
@@ -8,6 +9,8 @@ public interface PromotionService {
     public Promotion create(Promotion promotion);
 
     public List<Promotion> getAll();
+
+	Promotion getById(PromotionPK id);
 
     //public Promotion getById(Long id);
 
