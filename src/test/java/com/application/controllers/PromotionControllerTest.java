124da7e05a09026df379795dfb28f5028959d187
Hafsa <elmaizi.hafsa.it@gmail.com>
2022-03-14 11:20:12 +0100

    Test endpoint 'Create Promotion'

--- src/test/java/com/application/controllers/PromotionControllerTest.java
+++ src/test/java/com/application/controllers/PromotionControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static org.mockito.Mockito.when;
@@ -52,38 +53,37 @@ public class PromotionControllerTest {
     @Test
     public void createPromotion() throws Exception{
 
-        Promotion newPromotion = new Promotion(
-                new PromotionPK("DOSI2022","2021-2022"),
-                new Enseignant(null, "saliouTest", "philipeTest", "H","DF", "France", "Brest", "2 Rue des archives", "test@email.com", "testubo@gmail.com", "0687965241","0263524196", "235641"),
-                "DOSITest",
-                24,
-                null,
-                null,
-                null,
-                "LC117B",
-                "RECH",
-                "commentaire"
-                );
+        String newPromotion = "{\"id\":" +
+                "{\"code_Formation\": \"M2DOSI\"," +
+                "\"annee_Universitaire\": \"2013-2014\"}," +
+                "\"sigle_Promotion\": \"DOSI2022\","+
+                "\"nb_Max_Etudiant\":\"50\"," +
+                "\"date_Reponse_Lp\":\"04-05-13\"," +
+                "\"date_Reponse_Lalp\":\"19-05-13\"," +
+                "\"date_Rentree\":\"07-09-13\"," +
+                "\"lieu_Rentree\":\"LC117B\"," +
+                "\"processus_Stage\": null," +
+                "\"commentaire\":\"Commentaire Test\"," +
+                "\"enseignant\": {" +
+                    "\"no_Enseignant\": 1," +
+                    "\"nom\": \"S\"," +
+                    "\"prenom\": \"P\"," +
+                    "\"sexe\": \"H\"," +
+                    "\"type\": \"MCF\"," +
+                    "\"pays\": \"FR\"," +
+                    "\"ville\": \"LE DRENNEC\"," +
+                    "\"adresse\": \"6 rue de l'eglise\"," +
+                    "\"email_Perso\": \"ps@gmail.com\"," +
+                    "\"email_Ubo\": \"ps@univ-brest.fr\"," +
+                    "\"mobile\": \"06.00.00.01.00\"," +
+                    "\"telephone\": \"02.98.01.69.74\"," +
+                    "\"code_Postal\": \"29860\"" +
+                "}"+
+                "}";
 
-
-        Object randomObj = new Object() {
-            public final String sigle_Promotion = "DOSITest";
-            public final String nb_Max_Etudiant="50";
-            public final String date_Reponse_Lp="04-05-13";
-            public final String date_Reponse_Lalp="19-05-13";
-            public final String date_Rentree="07-09-13";
-            public final String lieu_Rentree="LC117B";
-            public final String processus_Stage="EC";
-            public final String commentaire="Commentaire Test";
-        };
-
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        String json = objectMapper.writeValueAsString(newPromotion);
-        System.out.println(json);
         mvc.perform(MockMvcRequestBuilders.post("http://localhost:9191/api/v1/promotions")
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON).content(json)
+                .accept(MediaType.APPLICATION_JSON).content(newPromotion)
                 .characterEncoding("utf-8")).andExpect(status().isOk());
     }
 
