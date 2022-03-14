f4d9f4786d1b7aef15585c54d9672b5968f55652
AKKAR Ferdaous <46265098+ferdaousak@users.noreply.github.com>
2022-03-14 16:15:15 +0100

    add salles endpoint

---
+++ src/main/java/com/application/models/Salle.java
@@ -0,0 +1,34 @@
+package com.application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="v_salle")
+public class Salle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonIgnore
+	private String code;
+	private String abreviation;
+	private String signification;
+}
--- src/test/java/com/application/ProjetAgileApplicationTests.java
+++ src/test/java/com/application/ProjetAgileApplicationTests.java
@@ -187,5 +187,6 @@ class ProjetAgileApplicationTests {
 	}
 	
 	
+	
 
 }
