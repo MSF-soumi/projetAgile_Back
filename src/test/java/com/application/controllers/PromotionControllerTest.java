ebe1a0e74f8ac5f9dc33cef8b41ac061a045d7d9
AKKAR Ferdaous <46265098+ferdaousak@users.noreply.github.com>
2022-03-17 15:14:02 +0100

    shit happened here

--- src/test/java/com/application/controllers/PromotionControllerTest.java
+++ src/test/java/com/application/controllers/PromotionControllerTest.java
@@ -18,33 +18,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PromotionControllerTest {
     @Autowired
     private MockMvc mvc;
-    
+
+
     @Test
     void contextLoads() {
     }
 
     @Test
-    public void getAllPromotions() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-    }
-
-    @Test
-    public void getPromotionById() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions/M2DOSI/2013-2014")
+    public void getAllPromotionById() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 
 
     @Test
-    public void createPromotion() throws Exception {
+    public void getPromotionById() throws Exception {
 
         String newPromotion = "{\"id\":" +
                 "{\"code_Formation\": \"M2DOSI\"," +
-                "\"annee_Universitaire\": \"2015-2016\"}," +
-                "\"sigle_Promotion\": \"DOSI2016\","+
+                "\"annee_Universitaire\": \"2014-2015\"}," +
+                "\"sigle_Promotion\": \"DOSI2022\","+
                 "\"nb_Max_Etudiant\":\"50\"," +
                 "\"date_Reponse_Lp\":\"04-05-13\"," +
                 "\"date_Reponse_Lalp\":\"19-05-13\"," +
@@ -54,17 +48,17 @@ public class PromotionControllerTest {
                 "\"commentaire\":\"Commentaire Test\"," +
                 "\"enseignant\": {" +
                     "\"no_Enseignant\": 1," +
-                    "\"nom\": \"SALIOU\"," +
-                    "\"prenom\": \"Philippe\"," +
+                    "\"nom\": \"S\"," +
+                    "\"prenom\": \"P\"," +
                     "\"sexe\": \"H\"," +
                     "\"type\": \"MCF\"," +
                     "\"pays\": \"FR\"," +
                     "\"ville\": \"LE DRENNEC\"," +
                     "\"adresse\": \"6 rue de l'eglise\"," +
-                    "\"email_Perso\": \"philippe.saliou@gmail.com\"," +
-                    "\"email_Ubo\": \"philippe.saliou@univ-brest.fr\"," +
-                    "\"mobile\": \"+33600000100\"," +
-                    "\"telephone\": \"+33298016974\"," +
+                    "\"email_Perso\": \"ps@gmail.com\"," +
+                    "\"email_Ubo\": \"ps@univ-brest.fr\"," +
+                    "\"mobile\": \"06.00.00.01.00\"," +
+                    "\"telephone\": \"02.98.01.69.74\"," +
                     "\"code_Postal\": \"29860\"" +
                 "}"+
                 "}";
@@ -79,14 +73,14 @@ public class PromotionControllerTest {
     public void createPromotionNoFK() throws Exception{
         String newPromotion = "{\"id\":" +
                 "{\"code_Formation\": \"M2DOSI\"," +
-                "\"annee_Universitaire\": \"2012-2013\"}," +
-                "\"sigle_Promotion\": \"DOSI13\","+
+                "\"annee_Universitaire\": \"2022-2023\"}," +
+                "\"sigle_Promotion\": \"DOSI2022\","+
                 "\"nb_Max_Etudiant\":\"50\"," +
                 "\"date_Reponse_Lp\":\"04-05-13\"," +
                 "\"date_Reponse_Lalp\":\"19-05-13\"," +
                 "\"date_Rentree\":\"07-09-13\"," +
                 "\"lieu_Rentree\":\"LC117B\"," +
-                "\"processus_Stage\": \"RECH\"," +
+                "\"processus_Stage\": null," +
                 "\"commentaire\":\"Commentaire Test\"" +
                 "}";
 
@@ -98,7 +92,7 @@ public class PromotionControllerTest {
 
     @Test
     public void deletePromotion() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.delete("http://localhost:9191/api/v1/promotions/M2DOSI/2010-2011")
+        mvc.perform(MockMvcRequestBuilders.delete("http://localhost:9191/api/v1/promotions/M2DOSI/2022-2023")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 
