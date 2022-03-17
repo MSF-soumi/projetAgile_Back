2bc8d4d3c47b964ae36e1579f67f70c12134b551
AKKAR Ferdaous <46265098+ferdaousak@users.noreply.github.com>
2022-03-17 15:07:49 +0100

    last change in here

--- src/test/java/com/application/controllers/PromotionControllerTest.java
+++ src/test/java/com/application/controllers/PromotionControllerTest.java
@@ -18,73 +18,75 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PromotionControllerTest {
     @Autowired
     private MockMvc mvc;
-
-
+    
     @Test
     void contextLoads() {
     }
 
     @Test
-    public void getAllPromotionById() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions/1")
+    public void getAllPromotions() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void getPromotionById() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions/M2DOSI/2013-2014")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 
 
-//    @Test
-//    public void getPromotionById() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("http://localhost:9191/api/v1/promotions/M2DOSI/2013-2014")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON).content(json)
-//                .characterEncoding("utf-8")).andExpect(status().isOk());
-//
-//        String newPromotion = "{\"id\":" +
-//                "{\"code_Formation\": \"M2DOSI\"," +
-//                "\"annee_Universitaire\": \"2014-2015\"}," +
-//                "\"sigle_Promotion\": \"DOSI2022\","+
-//                "\"nb_Max_Etudiant\":\"50\"," +
-//                "\"date_Reponse_Lp\":\"04-05-13\"," +
-//                "\"date_Reponse_Lalp\":\"19-05-13\"," +
-//                "\"date_Rentree\":\"07-09-13\"," +
-//                "\"lieu_Rentree\":\"LC117B\"," +
-//                "\"processus_Stage\": null," +
-//                "\"commentaire\":\"Commentaire Test\"," +
-//                "\"enseignant\": {" +
-//                    "\"no_Enseignant\": 1," +
-//                    "\"nom\": \"S\"," +
-//                    "\"prenom\": \"P\"," +
-//                    "\"sexe\": \"H\"," +
-//                    "\"type\": \"MCF\"," +
-//                    "\"pays\": \"FR\"," +
-//                    "\"ville\": \"LE DRENNEC\"," +
-//                    "\"adresse\": \"6 rue de l'eglise\"," +
-//                    "\"email_Perso\": \"ps@gmail.com\"," +
-//                    "\"email_Ubo\": \"ps@univ-brest.fr\"," +
-//                    "\"mobile\": \"06.00.00.01.00\"," +
-//                    "\"telephone\": \"02.98.01.69.74\"," +
-//                    "\"code_Postal\": \"29860\"" +
-//                "}"+
-//                "}";
-//
-//        mvc.perform(MockMvcRequestBuilders.post("http://localhost:9191/api/v1/promotions")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON).content(newPromotion)
-//                .characterEncoding("utf-8")).andExpect(status().isOk());
-//    }
-
     @Test
-    public void createPromotionNoFK() throws Exception{
+    public void createPromotion() throws Exception {
+
         String newPromotion = "{\"id\":" +
                 "{\"code_Formation\": \"M2DOSI\"," +
-                "\"annee_Universitaire\": \"2022-2023\"}," +
-                "\"sigle_Promotion\": \"DOSI2022\","+
+                "\"annee_Universitaire\": \"2015-2016\"}," +
+                "\"sigle_Promotion\": \"DOSI2016\","+
                 "\"nb_Max_Etudiant\":\"50\"," +
                 "\"date_Reponse_Lp\":\"04-05-13\"," +
                 "\"date_Reponse_Lalp\":\"19-05-13\"," +
                 "\"date_Rentree\":\"07-09-13\"," +
                 "\"lieu_Rentree\":\"LC117B\"," +
                 "\"processus_Stage\": null," +
+                "\"commentaire\":\"Commentaire Test\"," +
+                "\"enseignant\": {" +
+                    "\"no_Enseignant\": 1," +
+                    "\"nom\": \"SALIOU\"," +
+                    "\"prenom\": \"Philippe\"," +
+                    "\"sexe\": \"H\"," +
+                    "\"type\": \"MCF\"," +
+                    "\"pays\": \"FR\"," +
+                    "\"ville\": \"LE DRENNEC\"," +
+                    "\"adresse\": \"6 rue de l'eglise\"," +
+                    "\"email_Perso\": \"philippe.saliou@gmail.com\"," +
+                    "\"email_Ubo\": \"philippe.saliou@univ-brest.fr\"," +
+                    "\"mobile\": \"+33600000100\"," +
+                    "\"telephone\": \"+33298016974\"," +
+                    "\"code_Postal\": \"29860\"" +
+                "}"+
+                "}";
+
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:9191/api/v1/promotions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON).content(newPromotion)
+                .characterEncoding("utf-8")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void createPromotionNoFK() throws Exception{
+        String newPromotion = "{\"id\":" +
+                "{\"code_Formation\": \"M2DOSI\"," +
+                "\"annee_Universitaire\": \"2012-2013\"}," +
+                "\"sigle_Promotion\": \"DOSI13\","+
+                "\"nb_Max_Etudiant\":\"50\"," +
+                "\"date_Reponse_Lp\":\"04-05-13\"," +
+                "\"date_Reponse_Lalp\":\"19-05-13\"," +
+                "\"date_Rentree\":\"07-09-13\"," +
+                "\"lieu_Rentree\":\"LC117B\"," +
+                "\"processus_Stage\": \"RECH\"," +
                 "\"commentaire\":\"Commentaire Test\"" +
                 "}";
 
@@ -96,7 +98,7 @@ public class PromotionControllerTest {
 
     @Test
     public void deletePromotion() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.delete("http://localhost:9191/api/v1/promotions/M2DOSI/2022-2023")
+        mvc.perform(MockMvcRequestBuilders.delete("http://localhost:9191/api/v1/promotions/M2DOSI/2010-2011")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 
