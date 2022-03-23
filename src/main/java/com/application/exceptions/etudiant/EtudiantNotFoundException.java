e5f400d4257ee46b86f089930d3136b61040aac6
DEV2 <mchachti.soumiya@gmail.com>
2022-03-23 09:56:08 +0100

    exception

--- src/main/java/com/application/exceptions/etudiant/EtudiantNotFoundException.java
+++ src/main/java/com/application/exceptions/etudiant/EtudiantNotFoundException.java
@@ -4,11 +4,11 @@ import org.springframework.util.StringUtils;
 
 public class EtudiantNotFoundException extends RuntimeException {
 
-	private static String generateMessage(String entity, Long id) {
+	private static String generateMessage(String entity, String id) {
 		return "Aucun étudiant n'existe pour l'identifiant : " + id + '.';
 	}
 	
-	public EtudiantNotFoundException(Class c, Long id) {
+	public EtudiantNotFoundException(Class c, String id) {
 		super(EtudiantNotFoundException.generateMessage(c.getSimpleName(), id));
 	}
 
