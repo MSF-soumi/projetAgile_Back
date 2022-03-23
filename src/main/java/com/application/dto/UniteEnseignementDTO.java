ce822a51894b8002fc86f78b0dfeac6f19f2f419
Hafsa <elmaizi.hafsa.it@gmail.com>
2022-03-23 15:23:10 +0100

    Unite Enseignant DTO Validation

--- src/main/java/com/application/dto/UniteEnseignementDTO.java
+++ src/main/java/com/application/dto/UniteEnseignementDTO.java
@@ -4,6 +4,9 @@ import com.application.models.Enseignant;
 import com.application.models.UniteEnseignementPK;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class UniteEnseignementDTO {
 
@@ -11,10 +14,15 @@ public class UniteEnseignementDTO {
 
     private Enseignant enseignant;
 
+    @NotBlank(message = "Le champs est obligatoire.")
+    @Size(min = 1, max = 64, message = "Le champs ne doit pas dépasser 64 caractères.")
     private String designation;
 
+    @NotBlank(message = "Le champs est obligatoire.")
+    @Size(min = 1, max = 3, message = "Le champs ne doit pas dépasser 255 caractères.")
     private String semestre;
 
+    @Size(min = 0, max = 255, message = "Le champs ne doit pas dépasser 255 caractères.")
     private String description;
 
     private int nbh_cm;
